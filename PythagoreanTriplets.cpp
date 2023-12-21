d3e458dc468c63cab88cf924df14c268e53865f0
CMDRJacksonXJ <154385810+CMDRJacksonXJ@users.noreply.github.com>
2023-12-20 21:38:59 -0600

    PythagoreanTriplets.cpp

--- PythagoreanTriplets.cpp
+++ PythagoreanTriplets.cpp
@@ -42,18 +42,10 @@ int main()
     }
     system("cls");
     cout << "Complete! Found " << tf << " triplets" << endl;
+    // Time Complexity: O(n^3)
+    // Efficiency: n = 100,000, t = ~1 day, 8 hours
 
     file.close();
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
